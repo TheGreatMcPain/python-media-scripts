91f827426dab3d2d2fa082cc13122e748ac75c54
TheGreatMcPain <sixsupersonic@gmail.com>
2020-06-07 01:30:25 -0500

    batchconvert: Add option to create nightmode tracks. (based on code from 'nightmode')

--- batchconvert/encode_extras.py
+++ batchconvert/encode_extras.py
@@ -4,6 +4,7 @@ import havsfunc as haf
 import json
 import os
 import shutil
+import time
 import psutil
 import subprocess as sp
 import xml.etree.cElementTree as ET
@@ -20,6 +21,8 @@ RESUME = 'resume-file'
 
 JAVA = "/usr/lib64/openjdk-11/bin/java"
 BDSUP2SUB = "/home/james/.local/share/bdsup2sub/BDSup2Sub.jar"
+
+MAXDB = '-0.5'
 
 
 def main():
@@ -58,6 +61,10 @@ def convertMKV(infoFile):
         status = writeResume("extracted")
         print()
     if "extracted" in status:
+        createNightmodeTracks(info)
+        status = writeResume("nightmode")
+        print()
+    if "nightmode" in status:
         prepForcedSubs(info)
         print()
         encodeVideo(info)
@@ -69,7 +76,7 @@ def convertMKV(infoFile):
         status = writeResume("merged")
         print()
     if "merged" in status:
-        print("This one is done.")
+        print("Done")
 
 
 def writeResume(status):
@@ -102,6 +109,22 @@ def mergeMKV(info):
             '0:' + track['language'], '--default-track',
             '0:' + track['default'], 'audio-' + track['id'] + '.' + extension
         ]
+
+        if "yes" in track['nightmode']:
+            if "flac" in track['nightmodeCodec']:
+                extension = 'flac'
+            else:
+                extension = 'm4a'
+            cmd += [
+                '--track-name', '0:' + track['nightmodeLoudnormName'],
+                '--language', '0:' + track['language'], '--default-track',
+                '0:' + track['default'],
+                'nightmode-loudnorm-' + track['id'] + '.' + extension,
+                '--track-name', '0:' + track['nightmodeDrcName'], '--language',
+                '0:' + track['language'], '--default-track',
+                '0:' + track['default'],
+                'nightmode-drc-' + track['id'] + '.' + extension
+            ]
 
     if 'subs' in info:
         for track in info['subs']:
@@ -229,6 +252,125 @@ def ffprobe(in_file):
                             encoding='utf-8')))
 
 
+def getSamplerate(inFile):
+    return ffprobe(inFile)['streams'][0]['sample_rate']
+
+
+def getMaxdB(inFile):
+    cmd = [
+        'ffmpeg', '-i', inFile, '-acodec', 'pcm_s16le', '-af', 'volumedetect',
+        '-f', 'null', 'null'
+    ]
+    p = sp.Popen(cmd,
+                 stdout=sp.PIPE,
+                 stderr=sp.STDOUT,
+                 universal_newlines=True)
+    for line in p.stdout:
+        line = line.rstrip()
+        if 'max_volume' in line:
+            temp = line
+    print()
+    return temp[temp.index(':') + 2:-3]
+
+
+def ffmpegAudio(cmd, inFile, trackid):
+    print("Total Duration : ", end='')
+    if trackid is not None:
+        tags = ffprobe(inFile)['streams'][int(trackid)]['tags']
+    else:
+        tags = ffprobe(inFile)['streams'][0]
+    if 'duration' in tags:
+        durationSec = int(tags['duration'].split('.')[0])
+        durationMili = tags['duration'].split('.')[1]
+        duration = time.strftime('%H:%M:%S', time.gmtime(durationSec))
+        duration += '.' + durationMili
+        print(duration)
+    else:
+        print(tags['DURATION-eng'])
+    for x in cmd:
+        print(x, end=' ')
+    print()
+    p = sp.Popen(cmd,
+                 stderr=sp.STDOUT,
+                 stdout=sp.PIPE,
+                 universal_newlines=True)
+    for line in p.stdout:
+        line = line.rstrip()
+        if 'size=' in line:
+            print(f'{line}\r', end='')
+    print()
+
+
+def flacToM4a(outFile):
+    print("Converting flac to m4a")
+    m4aFile = outFile.split('.flac')[0] + '.m4a'
+    cmd = [
+        'ffmpeg', '-i', outFile, '-acodec', 'aac', '-b:a', '256K', '-movflags',
+        'faststart', '-y', m4aFile
+    ]
+    ffmpegAudio(cmd, outFile, None)
+    os.remove(outFile)
+
+
+def normAudio(inFile, outFile, codec, maxdB):
+    maxVolume = getMaxdB(inFile)
+    if maxVolume != '0.0':
+        volumeAdj = float(maxdB) - float(maxVolume)
+    else:
+        print("Already Normalized")
+        return False
+    print("Adjusting Volume by:", volumeAdj)
+    cmd = [
+        'ffmpeg', '-y', '-i', inFile, '-acodec', 'flac', '-compression_level',
+        '8', '-af', 'volume=' + str(volumeAdj) + 'dB', outFile
+    ]
+    ffmpegAudio(cmd, inFile, None)
+    verifyVol = getMaxdB(outFile)
+    if verifyVol == maxdB:
+        print("Normalize Complete")
+        if codec == 'aac':
+            flacToM4a(outFile)
+        return True
+    else:
+        print("Volume doesn't match desired result.")
+        exit()
+
+
+def nightmodeTrack(inFile, outFile, codec, withDRC, maxdB):
+    normfile = 'prenorm.flac'
+    filter = 'pan=stereo|FL=FC+0.30*FL+0.30*FLC+0.30*BL+0.30*SL+0.60*LFE|'
+    filter += 'FR=FC+0.30*FR+0.30*FRC+0.30*BR+0.30*SR+0.60*LFE,'
+    if withDRC:
+        filter += 'acompressor=ratio=4,loudnorm'
+    else:
+        filter += 'loudnorm'
+    samplerate = getSamplerate(inFile)
+    cmd = [
+        'ffmpeg', '-i', inFile, '-acodec', 'flac', '-compression_level', '8',
+        '-af', filter, '-ar', samplerate, '-y', normfile
+    ]
+    ffmpegAudio(cmd, inFile, None)
+    normalized = normAudio(normfile, outFile, codec, maxdB)
+    if normalized:
+        os.remove(normfile)
+
+
+def createNightmodeTracks(info):
+    audio = info['audio']
+    for track in audio:
+        if "yes" in track['nightmode']:
+            print('Creating nightmode tracks for trackid:', track['id'])
+            codec = track['nightmodeCodec']
+            extension = track['extension']
+            inFile = 'audio-' + track['id'] + '.' + extension
+            loudnormFile = 'nightmode-loudnorm-' + track['id'] + '.flac'
+            DRCFile = 'nightmode-drc-' + track['id'] + '.flac'
+            print("Creating 'Loudnorm' track.")
+            nightmodeTrack(inFile, loudnormFile, codec, True, MAXDB)
+            print("Creating 'DRC+Loudnorm' track.")
+            nightmodeTrack(inFile, DRCFile, codec, True, MAXDB)
+
+
 def extractTracks(info):
     sourceFile = info['sourceFile']
     audio = info['audio']
@@ -246,24 +388,15 @@ def extractTracks(info):
             cmd += ['audio-' + track['id'] + '.' + extension]
 
             print("Converting Audio via ffmpeg")
-            print("Total Duration: ", end='')
-            print(
-                ffprobe(sourceFile)['streams'][int(
-                    track['id'])]['tags']['DURATION-eng'])
-            p = sp.Popen(cmd,
-                         stderr=sp.STDOUT,
-                         stdout=sp.PIPE,
-                         universal_newlines=True)
-            for line in p.stdout:
-                line = line.rstrip()
-                if 'size=' in line:
-                    print(f'{line}\r', end='')
-            print()
+            ffmpegAudio(cmd, sourceFile, track['id'])
 
     cmd = ['mkvextract', sourceFile, 'tracks']
     for track in audio:
-        extension = track['extension']
-        cmd += [track['id'] + ':' + 'audio-' + track['id'] + '.' + extension]
+        if "no" in track['convert']:
+            extension = track['extension']
+            cmd += [
+                track['id'] + ':' + 'audio-' + track['id'] + '.' + extension
+            ]
 
     if subs != 0:
         for track in subs:
