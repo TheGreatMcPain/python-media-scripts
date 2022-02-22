36898ac977c54a05d46d73e96310356b812585e4
TheGreatMcPain <james@thegreatmcpain.xyz>
2022-02-22 02:26:42 -0600

    create-nightmode: Make defaults more closer to ffmpeg -ac 2
    Although the previous nightmode mix does improve clearity of voices
    the difference between center and other channels is very
    aggressive to the point where music is way too quiet for my taste, and
    I've noticed many movies will use the center channel for other things
    besides dialog (such as explosions).
    
    This new mix fits more closely with ATSC's downmix standard, and
    includes LFE. The loudnorm and DRC filters should still help with keeping
    the dymanics in-check.

--- nightmode/create-nightmode.py
+++ nightmode/create-nightmode.py
@@ -7,11 +7,10 @@ import getopt
 import subprocess as sp
 
 # Nightmode Downmixing settings.
-SUR_CHANNEL_VOL = 0.60  # Volume level to set the non-center channels to.
-LFE_CHANNEL_VOL = 0.60  # Volume to set the LFE channel to.
-
-# Globals
-MAXDB = '-0.5'
+SUR_CHANNEL_VOL = 0.707  # Volume level to set the non-center channels to.
+LFE_CHANNEL_VOL = 1.0  # Volume to set the LFE channel to.
+CENTER_CHANNEL_VOL = 1.0  # Volume to set the center channel to.
+MAXDB = "-0.5"
 
 
 def main():
@@ -19,15 +18,16 @@ def main():
     fileIn = None
 
     try:
-        options, args = getopt.getopt(sys.argv[1:], "hc:i:",
-                                      ["help", "codec=", "input="])
+        options, args = getopt.getopt(
+            sys.argv[1:], "hc:i:", ["help", "codec=", "input="]
+        )
         for name, value in options:
-            if name in ('-h', '--help'):
+            if name in ("-h", "--help"):
                 Usage()
                 sys.exit()
-            if name in ('-c', '--codec'):
+            if name in ("-c", "--codec"):
                 codec = value
-            if name in ('-i', '--input'):
+            if name in ("-i", "--input"):
                 fileIn = value
     except getopt.GetoptError as err:
         print(str(err))
@@ -45,14 +45,13 @@ def main():
         print("Error: Trailing arguments")
         sys.exit(1)
 
-    ext = fileIn.split('.')[-1]
+    ext = fileIn.split(".")[-1]
 
     createNightmodeTracks(codec, ext, fileIn)
 
 
 def Usage():
-    print("Usage:", sys.argv[0],
-          "-c,--codec <flac,aac> -i,--input <input file>")
+    print("Usage:", sys.argv[0], "-c,--codec <flac,aac> -i,--input <input file>")
 
 
 # from: https://github.com/Tatsh/ffmpeg-progress/blob/master/ffmpeg_progress.py
@@ -60,100 +59,135 @@ def ffprobe(in_file):
     """ffprobe font-end."""
     return dict(
         json.loads(
-            sp.check_output(('ffprobe', '-v', 'quiet', '-print_format', 'json',
-                             '-show_format', '-show_streams', in_file),
-                            encoding='utf-8')))
+            sp.check_output(
+                (
+                    "ffprobe",
+                    "-v",
+                    "quiet",
+                    "-print_format",
+                    "json",
+                    "-show_format",
+                    "-show_streams",
+                    in_file,
+                ),
+                encoding="utf-8",
+            )
+        )
+    )
 
 
 def getSamplerate(inFile):
-    return ffprobe(inFile)['streams'][0]['sample_rate']
+    return ffprobe(inFile)["streams"][0]["sample_rate"]
 
 
 def getMaxdB(inFile):
     cmd = [
-        'ffmpeg', '-i', inFile, '-acodec', 'pcm_s16le', '-af', 'volumedetect',
-        '-f', 'null', 'null'
+        "ffmpeg",
+        "-i",
+        inFile,
+        "-acodec",
+        "pcm_s16le",
+        "-af",
+        "volumedetect",
+        "-f",
+        "null",
+        "null",
     ]
-    p = sp.Popen(cmd,
-                 stdout=sp.PIPE,
-                 stderr=sp.STDOUT,
-                 universal_newlines=True)
+    p = sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.STDOUT, universal_newlines=True)
     for line in p.stdout:
         line = line.rstrip()
-        if 'max_volume' in line:
+        if "max_volume" in line:
             temp = line
     print()
-    return temp[temp.index(':') + 2:-3]
+    return temp[temp.index(":") + 2 : -3]
 
 
 def ffmpegAudio(cmd, inFile, trackid):
-    print("Total Duration : ", end='')
+    print("Total Duration : ", end="")
     if trackid is not None:
-        tags = ffprobe(inFile)['streams'][int(trackid)]['tags']
+        tags = ffprobe(inFile)["streams"][int(trackid)]["tags"]
     else:
-        tags = ffprobe(inFile)['streams'][0]
-    if 'duration' in tags:
-        durationSec = int(tags['duration'].split('.')[0])
-        durationMili = tags['duration'].split('.')[1]
-        duration = time.strftime('%H:%M:%S', time.gmtime(durationSec))
-        duration += '.' + durationMili
+        tags = ffprobe(inFile)["streams"][0]
+    if "duration" in tags:
+        durationSec = int(tags["duration"].split(".")[0])
+        durationMili = tags["duration"].split(".")[1]
+        duration = time.strftime("%H:%M:%S", time.gmtime(durationSec))
+        duration += "." + durationMili
         print(duration)
     else:
-        print(tags['DURATION-eng'])
+        print(tags["DURATION-eng"])
     for x in cmd:
-        print(x, end=' ')
+        print(x, end=" ")
     print()
-    p = sp.Popen(cmd,
-                 stderr=sp.STDOUT,
-                 stdout=sp.PIPE,
-                 universal_newlines=True)
+    p = sp.Popen(cmd, stderr=sp.STDOUT, stdout=sp.PIPE, universal_newlines=True)
     for line in p.stdout:
         line = line.rstrip()
-        if 'size=' in line:
-            print(f'{line}\r', end='')
+        if "size=" in line:
+            print(f"{line}\r", end="")
     print()
 
 
 def flacToM4a(outFile):
     print("Converting flac to m4a")
-    m4aFile = outFile.split('.flac')[0] + '.m4a'
+    m4aFile = outFile.split(".flac")[0] + ".m4a"
     cmd = [
-        'ffmpeg', '-i', outFile, '-acodec', 'aac', '-b:a', '256K', '-movflags',
-        'faststart', '-y', m4aFile
+        "ffmpeg",
+        "-i",
+        outFile,
+        "-acodec",
+        "aac",
+        "-b:a",
+        "256K",
+        "-movflags",
+        "faststart",
+        "-y",
+        m4aFile,
     ]
     ffmpegAudio(cmd, outFile, None)
     os.remove(outFile)
 
 
-def getffFilter(surVol: float, lfeVol: float):
+def getffFilter(surVol: float, lfeVol: float, centerVol: float):
     surVol = "{}".format(surVol)
-    lfeVol = "{}".format(lfeVol)
+    lfeVol = "{}".format(lfeVol / 2)
+    centerVol = "{}".format(centerVol / 2)
 
-    ffPanFilterL = 'FL=FC+{s}*FL+{s}*FLC+{s}*BL+{s}*SL+{l}*LFE'.format(
-        s=surVol, l=lfeVol)
-    ffPanFilterR = 'FR=FC+{s}*FR+{s}*FRC+{s}*BR+{s}*SR+{l}*LFE'.format(
-        s=surVol, l=lfeVol)
+    ffPanFilterL = "FL={c}*FC+{s}*FL+{s}*FLC+{s}*BL+{s}*SL+{l}*LFE".format(
+        c=centerVol, s=surVol, l=lfeVol
+    )
+    ffPanFilterR = "FR={c}*FC+{s}*FR+{s}*FRC+{s}*BR+{s}*SR+{l}*LFE".format(
+        c=centerVol, s=surVol, l=lfeVol
+    )
 
-    return 'pan=stereo|{}|{}'.format(ffPanFilterL, ffPanFilterR)
+    return "pan=stereo|{}|{}".format(ffPanFilterL, ffPanFilterR)
 
 
 def normAudio(inFile, outFile, codec, maxdB):
     maxVolume = getMaxdB(inFile)
-    if maxVolume != '0.0':
+    if maxVolume != "0.0":
         volumeAdj = float(maxdB) - float(maxVolume)
     else:
         print("Already Normalized")
         return False
     print("Adjusting Volume by:", volumeAdj)
     cmd = [
-        'ffmpeg', '-y', '-i', inFile, '-acodec', 'flac', '-compression_level',
-        '8', '-af', 'volume=' + str(volumeAdj) + 'dB', outFile
+        "ffmpeg",
+        "-y",
+        "-i",
+        inFile,
+        "-acodec",
+        "flac",
+        "-compression_level",
+        "8",
+        "-af",
+        "volume=" + str(volumeAdj) + "dB",
+        outFile,
     ]
     ffmpegAudio(cmd, inFile, None)
     verifyVol = getMaxdB(outFile)
     if verifyVol == maxdB:
         print("Normalize Complete")
-        if codec == 'aac':
+        if codec == "aac":
             flacToM4a(outFile)
         return True
     else:
@@ -161,17 +195,28 @@ def normAudio(inFile, outFile, codec, maxdB):
         exit()
 
 
-def nightmodeTrack(inFile, outFile, codec, withDRC, maxdB):
-    normfile = 'prenorm.flac'
-    ffFilter = getffFilter(SUR_CHANNEL_VOL, LFE_CHANNEL_VOL)
+def nightmodeTrack(inFile, outFile, codec, withLoudNorm, withDRC, maxdB):
+    normfile = "prenorm.flac"
+    ffFilter = getffFilter(SUR_CHANNEL_VOL, LFE_CHANNEL_VOL, CENTER_CHANNEL_VOL)
     if withDRC:
-        ffFilter += ',acompressor=ratio=4,loudnorm'
-    else:
-        ffFilter += ',loudnorm'
+        ffFilter += ",acompressor=ratio=4"
+    if withLoudNorm:
+        ffFilter += ",loudnorm"
     samplerate = getSamplerate(inFile)
     cmd = [
-        'ffmpeg', '-i', inFile, '-acodec', 'flac', '-compression_level', '8',
-        '-af', ffFilter, '-ar', samplerate, '-y', normfile
+        "ffmpeg",
+        "-i",
+        inFile,
+        "-acodec",
+        "flac",
+        "-compression_level",
+        "8",
+        "-af",
+        ffFilter,
+        "-ar",
+        samplerate,
+        "-y",
+        normfile,
     ]
     ffmpegAudio(cmd, inFile, None)
     normalized = normAudio(normfile, outFile, codec, maxdB)
@@ -180,16 +225,18 @@ def nightmodeTrack(inFile, outFile, codec, withDRC, maxdB):
 
 
 def createNightmodeTracks(codec, ext, inFile):
-    print('Creating nightmode tracks for:', inFile)
+    print("Creating nightmode tracks for:", inFile)
     extension = ext
     inFile = inFile
-    loudnormFile = inFile.split('.' +
-                                extension)[0] + '-nightmode-loudnorm.flac'
-    DRCFile = inFile.split('.' + extension)[0] + '-nightmode-drc.flac'
+    downmixFile = inFile.split("." + extension)[0] + "-nightmode.flac"
+    loudnormFile = inFile.split("." + extension)[0] + "-nightmode-loudnorm.flac"
+    DRCFile = inFile.split("." + extension)[0] + "-nightmode-drc.flac"
+    print("Creating 'DownmixOnly' track.")
+    nightmodeTrack(inFile, downmixFile, codec, False, False, MAXDB)
     print("Creating 'Loudnorm' track.")
-    nightmodeTrack(inFile, loudnormFile, codec, False, MAXDB)
+    nightmodeTrack(inFile, loudnormFile, codec, True, False, MAXDB)
     print("Creating 'DRC+Loudnorm' track.")
-    nightmodeTrack(inFile, DRCFile, codec, True, MAXDB)
+    nightmodeTrack(inFile, DRCFile, codec, True, True, MAXDB)
 
 
 if __name__ == "__main__":
